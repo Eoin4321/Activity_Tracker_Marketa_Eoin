bbf56cf1ca084ad58afd78c8960517f11fd964fe
Eoin4321 <eoinhamill1919@gmail.com>
2023-10-31 21:18:18 +0000

    Saved data taken from the csv file to variables. Created a method to check for spaces to fix errors taking in data.

--- src/Activity_Tracker_Package/Main.java
+++ src/Activity_Tracker_Package/Main.java
@@ -1,16 +1,44 @@
 package Activity_Tracker_Package;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    //Creating an ArrayList
+    ArrayList<ActivityTracker> stats = new ArrayList<>();
+
     public static void main(String[] args) throws Exception{
-        Scanner sc = new Scanner(new File("C:\\Users\\bilam\\Documents\\Lab Project\\Activity_Tracker_Marketa_Eoin\\src\\TestData\\activity_data_10.csv"));
+        Scanner sc = new Scanner(new File("C:\\Users\\kay\\Desktop\\OOPAssignment1\\Activity_Tracker\\src\\TestData\\activity_data_10.csv"));
         sc.useDelimiter(",");
 
+        String type;
+        int duration;
+        String date;
+        double distance;
+        double averageHeartRate;
+        String value;
+
+        System.out.println(sc.nextLine());
         while(sc.hasNext())
         {
-            System.out.print(sc.next());
+
+            type=sc.next();
+            date=sc.next();
+            value=sc.next();
+            duration=Integer.parseInt(value.substring(SpaceCount(value)));
+            System.out.println("duration added"+duration);
+
+            value=sc.next();
+            distance=Double.parseDouble(value.substring(SpaceCount(value)));
+            System.out.println("Distance value added "+distance);
+
+            value=sc.nextLine().substring(1);
+            System.out.println("Current value before error "+value);
+            averageHeartRate=Integer.parseInt(value.substring(SpaceCount(value)));
+
+            System.out.println("\nTYPE = "+type+"\nDuration = "+duration+"\nDate = "+date+"\nTYPE = "+distance+"\nTYPE = "+averageHeartRate);
+
         }
         sc.close();
     }
@@ -25,88 +53,58 @@ public class Main {
 
     public void Intensity(double kilometresPerHour, String type){
 
-        if(type=="Swimming")
-        {
-             if(kilometresPerHour<=0.5)
-             {
-                intensity = Activity.VERY_LIGHT;
-             }
-
-             else if(kilometresPerHour<=1.25)
-             {
-                 intensity = Activity.LIGHT;
-             }
-
-             else if(kilometresPerHour<= 2)
-             {
-                 intensity = Activity.MODERATE;
-             }
-
-             else if(kilometresPerHour<= 2.75)
-             {
-                intensity = Activity.VIGOROUS;
-             }
-
-             else if(kilometresPerHour<= 2.75)
-             {
-                intensity = Activity.VERY_VIGOROUS;
-             }
-        }
-
-        else if(type=="Running")
-        {
-            if(kilometresPerHour==4)
-            {
-                intensity = Activity.VERY_LIGHT;
+        switch (type) {
+            case "Swimming" -> {
+                if (kilometresPerHour <= 0.5) {
+                    intensity = Activity.VERY_LIGHT;
+                } else if (kilometresPerHour <= 1.25) {
+                    intensity = Activity.LIGHT;
+                } else if (kilometresPerHour <= 2) {
+                    intensity = Activity.MODERATE;
+                } else if (kilometresPerHour <= 2.75) {
+                    intensity = Activity.VIGOROUS;
+                } else if (kilometresPerHour <= 2.75) {
+                    intensity = Activity.VERY_VIGOROUS;
+                }
             }
-
-            else if(kilometresPerHour>4 && kilometresPerHour <=8)
-            {
-                intensity = Activity.LIGHT;
+            case "Running" -> {
+                if (kilometresPerHour == 4) {
+                    intensity = Activity.VERY_LIGHT;
+                } else if (kilometresPerHour > 4 && kilometresPerHour <= 8) {
+                    intensity = Activity.LIGHT;
+                } else if (kilometresPerHour > 8 && kilometresPerHour <= 12) {
+                    intensity = Activity.MODERATE;
+                } else if (kilometresPerHour > 12 && kilometresPerHour <= 16) {
+                    intensity = Activity.VIGOROUS;
+                } else if (kilometresPerHour > 16 && kilometresPerHour <= 24) {
+                    intensity = Activity.VERY_VIGOROUS;
+                }
             }
-
-            else if(kilometresPerHour>8 && kilometresPerHour <=12)
-            {
-                intensity = Activity.MODERATE;
-            }
-
-            else if(kilometresPerHour>12 && kilometresPerHour <=16)
-            {
-                intensity = Activity.VIGOROUS;
-            }
-
-            else if(kilometresPerHour>16 && kilometresPerHour <=24)
-            {
-                intensity = Activity.VERY_VIGOROUS;
+            case "Cycling" -> {
+                if (kilometresPerHour == 8) {
+                    intensity = Activity.VERY_LIGHT;
+                } else if (kilometresPerHour > 8 && kilometresPerHour <= 16) {
+                    intensity = Activity.LIGHT;
+                } else if (kilometresPerHour >= 17 && kilometresPerHour <= 25) {
+                    intensity = Activity.MODERATE;
+                } else if (kilometresPerHour > 25 && kilometresPerHour <= 33) {
+                    intensity = Activity.VIGOROUS;
+                } else if (kilometresPerHour > 33 && kilometresPerHour <= 40) {
+                    intensity = Activity.VERY_VIGOROUS;
+                }
             }
         }
+    }
 
-        else if(type=="Cycling")
-        {
-            if(kilometresPerHour==8)
-            {
-                intensity = Activity.VERY_LIGHT;
-            }
-
-            else if(kilometresPerHour>8 && kilometresPerHour <=16)
-            {
-                intensity = Activity.LIGHT;
-            }
-
-            else if(kilometresPerHour>=17 && kilometresPerHour <=25)
-            {
-                intensity = Activity.MODERATE;
-            }
-
-            else if(kilometresPerHour>25 && kilometresPerHour <=33)
-            {
-                intensity = Activity.VIGOROUS;
-            }
-
-            else if(kilometresPerHour>33 && kilometresPerHour <=40)
-            {
-                intensity = Activity.VERY_VIGOROUS;
+    public static int SpaceCount(String value)
+    {
+        int spaceCount=0;
+        for (int i = 0; i < value.length(); i++) {
+            // Check if the character at the current index is a space
+            if (value.charAt(i) == ' ') {
+                spaceCount++;
             }
         }
+        return spaceCount;
     }
 }
