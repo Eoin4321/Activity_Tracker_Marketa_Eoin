6b39a43448f81ad198dd739b6f2a882006e6a27c
grimmy962 <145358834+grimmy962@users.noreply.github.com>
2023-11-07 16:50:25 +0000

    added energyExpended(), aboveMinimumDistance()

--- src/Activity_Tracker_Package/Main.java
+++ src/Activity_Tracker_Package/Main.java
@@ -18,26 +18,15 @@ public class Main {
         //displayMenu(stats);
 
     }
-
-
-    Activity intensity;
-    public double kilometresPerHour(int duration, double distance){
-
-        double kilometresPerHour;
-        kilometresPerHour = duration/distance;
-        return kilometresPerHour;
-    }
-
-    public static void fileReader( ArrayList<ActivityTracker> stats ) throws Exception {
-        //Creating a scanner type keyboard called keyboard.
+    //File Reader Method
+    public static void fileReader() throws Exception {
         Scanner keyboard = new Scanner(System.in);
-        //Creating Variable to use to direct to the filepath
+        //Creating a string called filename which the user will input the name of the file into
         String filename;
-
-        System.out.println("Please type in the file location for the file you would to read.(Only type in the relative path and don't include .csv) ");
-        //Setting the filename String to the users input.
-        filename=keyboard.next();
-        Scanner sc = new Scanner("src/TestData/"+new File(filename)+".csv");
+        filename= keyboard.next();
+        System.out.println("Please type in the file you want to read from.");
+        //Chosing the file the user inputed
+        Scanner sc = new Scanner(new File("src\\TestData\\"+filename+".csv"));
         sc.useDelimiter(",");
         String type;
         int duration;
@@ -45,36 +34,26 @@ public class Main {
         double distance;
         double averageHeartRate;
         String value;
-        //Removing the stat labels at the top
         System.out.println(sc.nextLine());
-        //Creating an ArrayList to store the new data
-        ArrayList<ActivityTracker> newData = new ArrayList<>();
         while(sc.hasNext())
         {
             type=sc.next().trim();
             date=sc.next().trim();
             value=sc.next().trim();
             duration=Integer.parseInt(value);
-            System.out.println("duration added"+duration);
             value=sc.next().trim();
             distance=Double.parseDouble(value);
-            System.out.println("Distance value added "+distance);
             value=sc.nextLine().trim().substring(1);
             averageHeartRate=Double.parseDouble(value);
-            System.out.println("\nType = "+type+"\nDuration = "+duration+"\nDate = "+date+"\nDistance = "+distance+"\nAverageHeartRate = "+averageHeartRate);
-
-
-            //For loop to check for dublicates
+            ActivityTracker newActivity = new ActivityTracker(type, duration, date, distance, averageHeartRate);
+            if(!stats.contains(newActivity)){
+                stats.add(newActivity);
+            }
         }
-
-
-
-
-
         sc.close();
     }
     //menu method
-    public void displayMenu(int menuNum,  ArrayList<ActivityTracker> stats) throws Exception{
+    public void displayMenu(int menuNum, ArrayList<ActivityTracker> stats) throws Exception {
         Scanner keyboard = new Scanner(System.in);
 
 //the menu
@@ -85,18 +64,17 @@ public class Main {
         System.out.println("(to navigate around the meu please type in number)");
 
 //if user types in 1 it will bring them to the viewFile method to view the files
-        if(menuNum == 1){
+        if (menuNum == 1) {
             viewFile();
         }
 
 //if 2 it will bring them to the fileReader where the user can upload a new file
-        else if(menuNum == 2){
-            fileReader(stats);
+        else if (menuNum == 2) {
+            fileReader();
         }
     }
-
     //viewing file method - an array list of how many files the user uploaded
-    public static void viewFile(){
+    public static void viewFile() {
         Scanner keyboard = new Scanner(System.in);
         String answer;
 
@@ -106,23 +84,19 @@ public class Main {
         System.out.println("1) Default\n");
         System.out.println("2) Subset\n");
         System.out.println("3) Overall Performance");
-        answer=keyboard.nextLine();
+        answer = keyboard.nextLine();
 
-        if(answer == "1"){
+        if (answer == "1") {
             defaultView();
-        }
-
-        else if(answer == "2"){
+        } else if (answer == "2") {
             subsetView();
-        }
-
-        else if(answer == "3"){
+        } else if (answer == "3") {
             overallView();
         }
 
     }
 
-    public static void defaultView(){
+    public static void defaultView() {
         Scanner keyboard = new Scanner(System.in);
         String answer;
 
@@ -132,27 +106,23 @@ public class Main {
                 "4) Type of activity\n" +
                 "5) Distance\n");
 
-        answer=keyboard.nextLine();
+        answer = keyboard.nextLine();
 
-        if(answer == "1"){
-            caloriesBurned();
-        }
-        else if(answer == "2"){
+        if (answer == "1") {
+            //caloriesBurned();
+        } else if (answer == "2") {
             date();
-        }
-        else if(answer == "3"){
+        } else if (answer == "3") {
             activityDuration();
-        }
-        else if(answer == "4"){
+        } else if (answer == "4") {
             activityType();
-        }
-        else if(answer == "5"){
+        } else if (answer == "5") {
             distance();
         }
 
     }
 
-    public static void subsetView(){
+    public static void subsetView() {
         Scanner keyboard = new Scanner(System.in);
         String answer;
 
@@ -160,9 +130,22 @@ public class Main {
                 "2) Above a minimum distance\n" +
                 "3) Type of energy expended\n" +
                 "4) Above a minimum duration");
+
+        answer = keyboard.nextLine();
+
+        if (answer == "1") {
+            activityType();
+        } else if (answer == "2") {
+            //aboveMinimumDistance();
+        } else if (answer == "3") {
+            energyExpended();
+        } else if (answer == "4") {
+            aboveMinimumDuration();
+
+        }
     }
 
-    public static void overallView(){
+    public static void overallView() {
         Scanner keyboard = new Scanner(System.in);
         String answer;
 
@@ -171,56 +154,45 @@ public class Main {
     }
 
 
-    public static void caloriesBurned(int duration, int kilometresPerHour){
+    public static void caloriesBurned(int duration, int kilometresPerHour) {
         Scanner keyboard = new Scanner(System.in);
         String answer;
         String activity;
 
         double caloriesBurned;
-        caloriesBurned = duration*kilometresPerHour;
-
+        caloriesBurned = duration * kilometresPerHour;
         //based on what should they view it? activity? - probs
         System.out.println("Based on what activity do you want to see: Calories Burned?");
-        answer=keyboard.nextLine();
+        answer = keyboard.nextLine();
 
-        if(answer == "Swimming")
-            {
+        if (answer == "Swimming") {
 
-                //get only the swimming column (get)
+            //get only the swimming column (get)
 
-                //sorts in ascending order
-                // Arrays.sort(ARRAY NAME);
+            //sorts in ascending order
+            // Arrays.sort(ARRAY NAME);
 
-                //sorts in reverse order
-                // reverse(ARRAY NAME);
-            }
-        else if(answer == "Cycling")
-            {
+            //sorts in reverse order
+            // reverse(ARRAY NAME);
+        } else if (answer == "Cycling") {
 
-            }
+        } else if (answer == "Running") {
 
-        else if(answer == "Running")
-            {
+        } else {
 
-            }
-
-        else
-            {
-
-            }
+        }
 
     }
 
-    public static void date(){
+    public static void date() {
         Scanner keyboard = new Scanner(System.in);
         String answer;
 
         //based on what should they view it? activity? - probs
         System.out.println("Based on what activity do you want to see: Date?");
-        answer=keyboard.nextLine();
+        answer = keyboard.nextLine();
 
-        if(answer == "Swimming")
-        {
+        if (answer == "Swimming") {
 
             //get only the swimming column (get)
 
@@ -234,60 +206,261 @@ public class Main {
             System.out.println("View in: 1) Ascending  OR   2)Descending order");
             input = keyboard.nextLine();
 
-            if(input == "1" || input =="Ascending")
-                {
-                    // Arrays.sort(ARRAY NAME);
-                    //sout the array
-                }
-
-            else if (input == "2" || input == "Descending")
-                {
-                    // Arrays.sort(ARRAY NAME);
-                    // reverse(ARRAY NAME);
-                    //sout the array
-                }
-        }
-
-        else if(answer == "Cycling")
-        {
+            if (input == "1" || input == "Ascending") {
+                // Arrays.sort(ARRAY NAME);
+                //sout the array
+            } else if (input == "2" || input == "Descending") {
+                // Arrays.sort(ARRAY NAME);
+                // reverse(ARRAY NAME);
+                //sout the array
+            }
+        } else if (answer == "Cycling") {
             String input;
             System.out.println("View in: 1) Ascending  OR   2)Descending order");
             input = keyboard.nextLine();
 
-            if(input == "1" || input =="Ascending")
-            {
+            if (input == "1" || input == "Ascending") {
                 // Arrays.sort(ARRAY NAME);
                 //sout the array
+            } else if (input == "2" || input == "Descending") {
+                // Arrays.sort(ARRAY NAME);
+                // reverse(ARRAY NAME);
+                //sout the array
             }
+        } else if (answer == "Running") {
+            String input;
+            System.out.println("View in: 1) Ascending  OR   2)Descending order");
+            input = keyboard.nextLine();
 
-            else if (input == "2" || input == "Descending")
-            {
+            if (input == "1" || input == "Ascending") {
+                // Arrays.sort(ARRAY NAME);
+                //sout the array
+            } else if (input == "2" || input == "Descending") {
                 // Arrays.sort(ARRAY NAME);
                 // reverse(ARRAY NAME);
                 //sout the array
             }
         }
+    }
 
-        else if(answer == "Running")
-        {
+    public static void activityDuration() {
+        Scanner keyboard = new Scanner(System.in);
+        String answer;
+
+        //based on what should they view it? activity? - probs
+        System.out.println("Based on what activity do you want to see: Activity Duration?");
+        answer = keyboard.nextLine();
+
+        if (answer == "Swimming") {
+
+            //get only the swimming column (get)
+
+            //sorts in ascending order
+            // Arrays.sort(ARRAY NAME);
+
+            //sorts in reverse order
+            // reverse(ARRAY NAME);
+
             String input;
             System.out.println("View in: 1) Ascending  OR   2)Descending order");
             input = keyboard.nextLine();
 
-            if(input == "1" || input =="Ascending")
-            {
+            if (input == "1" || input == "Ascending") {
                 // Arrays.sort(ARRAY NAME);
                 //sout the array
+            } else if (input == "2" || input == "Descending") {
+                // Arrays.sort(ARRAY NAME);
+                // reverse(ARRAY NAME);
+                //sout the array
             }
+        } else if (answer == "Cycling") {
+            String input;
+            System.out.println("View in: 1) Ascending  OR   2)Descending order");
+            input = keyboard.nextLine();
 
-            else if (input == "2" || input == "Descending")
-            {
+            if (input == "1" || input == "Ascending") {
+                // Arrays.sort(ARRAY NAME);
+                //sout the array
+            } else if (input == "2" || input == "Descending") {
+                // Arrays.sort(ARRAY NAME);
+                // reverse(ARRAY NAME);
+                //sout the array
+            }
+        } else if (answer == "Running") {
+            String input;
+            System.out.println("View in: 1) Ascending  OR   2)Descending order");
+            input = keyboard.nextLine();
+
+            if (input == "1" || input == "Ascending") {
+                // Arrays.sort(ARRAY NAME);
+                //sout the array
+            } else if (input == "2" || input == "Descending") {
                 // Arrays.sort(ARRAY NAME);
                 // reverse(ARRAY NAME);
                 //sout the array
             }
         }
+    }
 
+    public static void distance() {
+        Scanner keyboard = new Scanner(System.in);
+        String answer;
+
+        //based on what should they view it? activity? - probs
+        System.out.println("Based on what activity do you want to see: Distance?");
+        answer = keyboard.nextLine();
+
+        if (answer == "Swimming") {
+
+            //get only the swimming column (get)
+
+            //sorts in ascending order
+            // Arrays.sort(ARRAY NAME);
+
+            //sorts in reverse order
+            // reverse(ARRAY NAME);
+
+            String input;
+            System.out.println("View in: 1) Ascending  OR   2)Descending order");
+            input = keyboard.nextLine();
+
+            if (input == "1" || input == "Ascending") {
+                // Arrays.sort(ARRAY NAME);
+                //sout the array
+            } else if (input == "2" || input == "Descending") {
+                // Arrays.sort(ARRAY NAME);
+                // reverse(ARRAY NAME);
+                //sout the array
+            }
+        } else if (answer == "Cycling") {
+            String input;
+            System.out.println("View in: 1) Ascending  OR   2)Descending order");
+            input = keyboard.nextLine();
+
+            if (input == "1" || input == "Ascending") {
+                // Arrays.sort(ARRAY NAME);
+                //sout the array
+            } else if (input == "2" || input == "Descending") {
+                // Arrays.sort(ARRAY NAME);
+                // reverse(ARRAY NAME);
+                //sout the array
+            }
+        } else if (answer == "Running") {
+            String input;
+            System.out.println("View in: 1) Ascending  OR   2)Descending order");
+            input = keyboard.nextLine();
+
+            if (input == "1" || input == "Ascending") {
+                // Arrays.sort(ARRAY NAME);
+                //sout the array
+            } else if (input == "2" || input == "Descending") {
+                // Arrays.sort(ARRAY NAME);
+                // reverse(ARRAY NAME);
+                //sout the array
+            }
+        }
+    }
+
+    public static void activityType() {
+        Scanner keyboard = new Scanner(System.in);
+        String answer;
+
+        //based on what should they view it? activity? - probs
+        System.out.println("Based on what activity do you want to see: Type of Activity?");
+        answer = keyboard.nextLine();
+
+        if (answer == "Swimming") {
+
+            //get only the swimming column (get)
+            //printout
+
+        }
+
+        else if(answer == "Running"){
+
+        }
+
+        else if(answer == "Cycling"){
+
+        }
+    }
+
+    public static void aboveMinimumDistance(ArrayList<ActivityTracker> stats){
+        Scanner keyboard = new Scanner(System.in);
+       // Collections.sort(stats);
+        int minDistance = Integer.MAX_VALUE;
+
+        System.out.println("What's your minimum distance requirement?");
+        minDistance = keyboard.nextInt();
+
+       for(int i=1; i<stats.size();i++)
+       {
+            if(minDistance>stats.get(i).distance)
+                {
+                    System.out.println(stats.toString());
+                }
+       }
+
+    }
+
+    public static void energyExpended(){
+        Scanner keyboard = new Scanner(System.in);
+
+        String gender;
+        int age;
+        double weight;
+        double bmr;
+
+        System.out.println("What gender are you?");
+        System.out.println("a) Male     OR      b) Female");
+        gender = keyboard.nextLine();
+
+        System.out.println("What age are you? (min 18, max 74)");
+        age = keyboard.nextInt();
+
+        System.out.println("How much do you weight?");
+        weight = keyboard.nextDouble();
+
+        if(gender=="Male" || gender=="a")
+            {
+                if(age>=18&&age<=29)
+                    {
+                        bmr = (0.063*weight+2.896)*238.85;
+                    }
+
+                else if(age>=30&&age<=59)
+                    {
+                        bmr = (0.048*weight+3.653)*238.85;
+                    }
+
+                else if(age>=60&&age<=74)
+                    {
+                        bmr = (0.0499*weight+2.930)*238.85;
+                    }
+            }
+
+        else if(gender=="Female" || gender == "b")
+            {
+                if(gender=="Male" || gender=="a")
+                    {
+                        bmr=(0.062*weight+2.036)*238.85;
+                    }
+
+                else if(age>=30&&age<=59)
+                    {
+                       bmr = (0.034*weight*3.538)*238.85;
+                    }
+
+                else if(age>=60&&age<=74)
+                {
+                    bmr = (0.0386*weight+2.875)*238.85;
+                }
+            }
+
+        System.out.println("The total energy expenditure is: "+bmr+" kcal.");
+    }
+
+    public static void aboveMinimumDuration(){
 
     }
 }
